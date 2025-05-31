94d1e7932edfd18986c749ee10388ec47d30b33a
dhaneesh <dhaneeshjadhav@gmail.com>
2025-06-01 00:06:25 +0900

    dhan

--- scripts/startserver.sh
+++ scripts/startserver.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
-##set -e  # Exit immediately on any error
+set -e  # Exit immediately on any error
 
-#echo "=== [startserver.sh] Starting Tomcat and HTTPD services ===" >> /tmp/deploy.log
+echo "=== [startserver.sh] Starting Tomcat and HTTPD services ===" >> /tmp/deploy.log
 
 # Start and enable Tomcat
-#sudo systemctl enable tomcat.service
-#sudo systemctl start tomcat.service
+sudo systemctl enable tomcat.service
+sudo systemctl start tomcat.service
 
 # Start and enable Apache HTTPD
-#udo systemctl enable httpd.service
-#sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
+sudo systemctl start httpd.service
 
-#echo "=== [startserver.sh] Services started successfully ===" >> /tmp/deploy.log
-#exit 0
-cd /opt/tomcat9/bin
-sudo ./startup.sh
+echo "=== [startserver.sh] Services started successfully ===" >> /tmp/deploy.log
+exit 0
--- scripts/stopserver.sh
+++ scripts/stopserver.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-#isexistapp="$(pgrep httpd)"
-##if [[ -n $isexistapp ]]; then 
-#udo systemctl stop httpd.service
-#fi
+isexistapp="$(pgrep httpd)"
+if [[ -n $isexistapp ]]; then 
+sudo systemctl stop httpd.service
+fi
 
-#isexistapp="$(pgrep tomcat)"
-#if [[ -n $isexistapp ]]; then 
-#sudo systemctl stop tomcat.service
-#fi
-
-cd /opt/tomcat9/bin
-sudo ./stopserver.sh
+isexistapp="$(pgrep tomcat)"
+if [[ -n $isexistapp ]]; then 
+sudo systemctl stop tomcat.service
+fi
